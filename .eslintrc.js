bbb64e271e24b1e1534d91ccd115664e9aa63d2c
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2021-09-13 16:55:29 +0300

    Config Webpack

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json',
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: 'google',
+  rules: {
+    'semi': 'off',
+    'comma-dangle': 'off',
+  }
+}
